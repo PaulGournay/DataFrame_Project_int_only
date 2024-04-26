a13c9102d70332b004d47a3c99d07eb9aba32fb1
Paul Gournay <151144120+PaulGournay@users.noreply.github.com>
2024-04-26 12:49:06 +0200

    Changes by Vrejk

--- cdataframe.c
+++ cdataframe.c
@@ -1,3 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "cdataframe.h"
+#include "column.c"
+#include "column.h"
+COLUMN** create_cdataframe(){
+    COLUMN **pt=(COLUMN **)malloc(sizeof(COLUMN));
+    return pt;
+};
+
